600024b484f1930f463ce6ff04e2e5dc3a485d9a
Iryna Howarth <irynahowarth@gmail.com>
2016-03-01 13:04:32 +0000

    restaurants can be edited and deleted

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,25 @@
 class RestaurantsController < ApplicationController
- 
+
   def index
     @restaurants = Restaurant.all
   end
 
   def new
-  end 
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+
+    if @restaurant.update(params.require(:restaurant).permit(:name, :rating))
+      redirect_to @restaurant
+    else
+      render 'edit'
+    end
+  end
 
   def create
     @restaurant = Restaurant.new(params.require(:restaurant).permit(:name, :rating))
@@ -16,6 +30,12 @@ class RestaurantsController < ApplicationController
 
   def show
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path
   end
 
 end
