7496d185c1d61b730ad771f46b50fac383bf022c
Iryna Howarth <irynahowarth@gmail.com>
2016-03-03 23:32:31 +0000

    add OmniAuth login with Facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
 
-devise_for :users
+devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
 resources :restaurants do
   resources :reviews
 end
 
-# get 'restaurants' => 'restaurants#index'
 
 root 'restaurants#index'
 
